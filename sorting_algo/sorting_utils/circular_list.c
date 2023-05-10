31102903a26cc76b6461ed01941e7d42460fdbef
TakrayoutMohamed <takrayoutm@gmail.com>
2023-05-10 07:10:08 +0100

    in this version i changed the way i passe values of stack b to a

--- sorting_algo/sorting_utils/circular_list.c
+++ sorting_algo/sorting_utils/circular_list.c
@@ -6,7 +6,7 @@
 /*   By: takra <takra@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 14:54:36 by takra             #+#    #+#             */
-/*   Updated: 2023/05/09 19:48:53 by takra            ###   ########.fr       */
+/*   Updated: 2023/05/09 23:09:33 by takra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,30 @@ static int	get_index_of_prior_node(t_list **lst)
 	return (index_of_prior);
 }
 
-void	sort_b_in_a(t_list **a, t_list **b)
+static int	get_position_of_prior_node(t_list **lst)
 {
+	int		moves_to_position;
+	int		position_of_prior;
+	t_list	*tmp;
 
+	tmp = *lst;
+	moves_to_position = 2147483647;
+	while (tmp != NULL)
+	{
+		if (tmp->position + tmp->index < moves_to_position)
+		{
+			moves_to_position = tmp->position + tmp->index;
+			position_of_prior = tmp->position;
+		}
+		tmp = tmp->next;
+	}
+	tmp = NULL;
+	return (position_of_prior);
 }
 
-static void	fill_position_of_b_in_a(t_list **b, t_list **a)
+static void	fill_position_of_b_in_a(t_list **a, t_list **b)
 {
 	t_list	*tmp;
-	int		i;
-	int		right_position;
 
 	tmp = *b;
 	if (ft_lstsize(*b) > 0)
@@ -50,6 +64,11 @@ static void	fill_position_of_b_in_a(t_list **b, t_list **a)
 		while (tmp != NULL)
 		{
 			tmp->position = get_right_position(*a, tmp->content);
+			// ft_putstr_fd("\nhere the position of the number ",1);
+			// ft_putnbr_fd(tmp->content ,1);
+			// ft_putstr_fd(" is  \n",1);
+			// ft_putnbr_fd(tmp->position ,1);
+			// ft_putstr_fd("\n",1);
 			tmp = tmp->next;
 		}
 	}
@@ -66,7 +85,7 @@ static void	fill_indexes_of_a_b(t_list **a, t_list **b)
 	{
 		while (tmp != NULL)
 		{
-			tmp->position = i++;
+			tmp->index = i++;
 			tmp = tmp->next;
 		}
 	}
@@ -76,30 +95,73 @@ static void	fill_indexes_of_a_b(t_list **a, t_list **b)
 	{
 		while (tmp != NULL)
 		{
-			tmp->position = i++;
+			tmp->index = i++;
 			tmp = tmp->next;
 		}
 	}
 }
 
+void	sort_b_in_a(t_list **a, t_list **b, int index, int position)
+{
+	if (position > 0 && index > 0)
+	{
+		if (position + index <= ft_lstsize(*a) / 2)
+		{
+			rr(a, b);
+		}
+		else
+		{
+			rrr(a, b);
+		}
+	}
+	else if (position == 0 && index != 0)
+	{
+		if (index > ft_lstsize(*b) / 2)
+		{
+			rrb(b);
+		}
+		else
+		{
+			rb(b);
+		}
+	}
+	else if (position != 0 && index == 0)
+	{
+		if (position > ft_lstsize(*a) / 2)
+		{
+			rra(a);
+		}
+		else
+		{
+			ra(a);
+		}
+	}
+}
 /*add elements of b to a in a circular sort*/
 void	circular_list(t_list **a, t_list **b)
 {
 	int	index_of_prior_node;
+	int	position_of_prior_node;
 
+	fill_position_of_b_in_a(a, b);
+	fill_indexes_of_a_b(a, b);
 	while (ft_lstsize(*b))
 	{
-		fill_position_of_b_in_a(a, b);
-		fill_indexes_of_a_b(a, b);
-		index_of_prior_node = get_index_of_prior_node(b);
-		while (index_of_prior_node > 0)
+		while (1)
 		{
-			if ((*b)->index != 0 && (*b)->position != 0)
+			index_of_prior_node = get_index_of_prior_node(b);
+			position_of_prior_node = get_position_of_prior_node(b);
+			// printf("p index = %d\np position = %d",index_of_prior_node,position_of_prior_node);
+			// exit(0);
+			fill_position_of_b_in_a(a, b);
+			fill_indexes_of_a_b(a, b);
+			if ((*b)->index == 0 && (*b)->position == 0)
 			{
+				pa(a, b);
 				break ;
 			}
-			sort_b_in_a(a, b);
-			index_of_prior_node--;
+			sort_b_in_a(a, b, index_of_prior_node, position_of_prior_node);
+			// index_of_prior_node--;
 		}
 	}
 }
